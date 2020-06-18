169414097940d64f730000e11798a575ae8fc134
Insaf <hamzin730insaf@gmail.com>
2020-06-18 15:28:09 -0400

    removed git from client

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+function CommentCreate({ postId }) {
+    const [content, setContent] = useState('');
+
+    const onSumbit = async (event) => {
+        event.preventDefault();
+        await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+            content
+        });
+        setContent('')
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSumbit}>
+                <div className="form-group">
+                    <label>New comment</label>
+                    <input
+                        className="form-control"
+                        value={content}
+                        onChange={e => setContent(e.target.value)}
+                    />
+                </div>
+                <button className="btn btn-primary">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default CommentCreate
---
+++ client/src/CommentList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function CommentList({ comments }) {
+
+    const renderedComments = comments.map(comment => {
+        let content;
+        if (comment.status === 'approved') {
+            content = comment.content
+        } else if (comment.status === 'pending') {
+            content = 'Awaiting moderation';
+        } else {
+            content = 'Rejected'
+        }
+
+        return (
+            <li key={comment.id}>{content}</li>
+        );
+    });
+
+    return (
+        <ul>
+            {renderedComments}
+        </ul>
+    )
+}
+
+export default CommentList
