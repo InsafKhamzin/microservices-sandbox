80c81c0e8b6595e7332e9ca7f90f42e0f9bcd4c0
insaf730hamzin <hamzin730insaf@gmail.com>
2020-06-20 18:08:16 -0400

    ingress and loadbalancer config

--- posts/index.js
+++ posts/index.js
@@ -9,11 +9,7 @@ const posts = {};
 app.use(express.json({ extended: false }))
 app.use(cors())
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
-
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
